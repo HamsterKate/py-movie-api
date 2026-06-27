f8d697a7252a64ade5d26f21bbe0482ffd9b0b00
Kate Po <hamster.k.po@gmail.com>
2026-06-27 15:20:17 +0300

    modify Movie model

--- cinema/models.py
+++ cinema/models.py
@@ -5,3 +5,9 @@ class Movie(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     duration = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = 'movies'
+
+    def __str__(self):
+        return f"{self.title} ({self.duration} mins)"

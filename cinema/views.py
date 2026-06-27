f1abaca6554d6a5118f3b7ea3015526f6821b900
Kate Po <hamster.k.po@gmail.com>
2026-06-27 16:21:04 +0300

    implement api views

--- cinema/views.py
+++ cinema/views.py
@@ -1,5 +1,41 @@
-def movie_list(request):
-    ...
+from django.core.serializers import serialize
+from django.http import JsonResponse
+from django.shortcuts import render
+from requests import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 
+from cinema.models import Movie
+from cinema.serializer import MovieSerializer
+
+
+@api_view(["GET", "POST"])
+def movie_list(request):
+    if request.method == "GET":
+        buses = Movie.objects.all()
+        serializer = MovieSerializer(buses, many=True)
+        return JsonResponse(serializer.data, safe=False, status=200)
+    else:
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk):
-    ...
+    movie = get_object_or_404(Movie, pk=pk)
+    if request.method == "GET":
+        serializer = MovieSerializer(movie)
+        return JsonResponse(serializer.data, status=200)
+    elif request.method == "PUT":
+        serializer = MovieSerializer(movie, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
